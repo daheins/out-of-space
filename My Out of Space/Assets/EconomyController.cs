819240061dfada2cb61c86a52abd64f7da204b6c
Brian Shih <bshih@korhal-2.local>
2018-08-11 18:20:33 -0700

    economy controller init

---
+++ My Out of Space/Assets/EconomyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EconomyController : MonoBehaviour {
+    public long currencyBalance;
+    public long income;
+    public Text currencyBalanceText;
+    public Text incomeText;
+
+	// Use this for initialization
+	void Start () {
+        currencyBalance = 0;
+        income = 1;
+        currencyBalanceText.text = "Currency: " + currencyBalance.ToString();
+        incomeText.text = "Income: " + income.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currencyBalance = currencyBalance + income;
+        currencyBalanceText.text = "Currency: " + currencyBalance.ToString();
+        incomeText.text = "Income: " + income.ToString();
+	}
+}
