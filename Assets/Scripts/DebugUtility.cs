98f9461d7f79f5f1d6a6dc6e00b64ae7f80f1704
Daniel Heins <dheins@pocketgems.com>
2018-08-12 14:13:16 -0700

    Buttons to zoom in and out (not centered ATM). Canvas shows above other game elements.

---
+++ Assets/Scripts/DebugUtility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugUtility : MonoBehaviour {
+
+    private int ZoomLevel = 0;
+
+    public GameObject factoryParent;
+
+    public void DidTapZoomIn () {
+        ZoomLevel++;
+        float scaleFactor = 1.4F;
+        factoryParent.transform.localScale = new Vector3(factoryParent.transform.localScale.x * scaleFactor,
+                                                         factoryParent.transform.localScale.y * scaleFactor,
+                                                         factoryParent.transform.localScale.z * scaleFactor);
+	}
+
+    public void DidTapZoomOut() {
+        ZoomLevel--;
+        float scaleFactor = .7F;
+        factoryParent.transform.localScale = new Vector3(factoryParent.transform.localScale.x * scaleFactor,
+                                                         factoryParent.transform.localScale.y * scaleFactor,
+                                                         factoryParent.transform.localScale.z * scaleFactor);
+    }
+}
