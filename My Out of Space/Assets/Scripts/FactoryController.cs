72412bf837c42cb5f7c66827eb637b19c082cb72
Daniel Heins <dheins@pocketgems.com>
2018-08-11 18:46:46 -0700

    Jennie factory prefab

--- My Out of Space/Assets/Scripts/FactoryController.cs
+++ My Out of Space/Assets/Scripts/FactoryController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class FactoryController : MonoBehaviour {
 
+    private int factoryLevel;
+    private Dictionary<int, int> levelToIncomeMap;
+
 	// Use this for initialization
 	void Start () {
-		
+        factoryLevel = 0;
+        levelToIncomeMap = new Dictionary<int, int> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public int Income () {
+        return levelToIncomeMap[factoryLevel];
+    }
+
+    public void LevelUpFactory () {
+        factoryLevel += 1;
+    }
 }
